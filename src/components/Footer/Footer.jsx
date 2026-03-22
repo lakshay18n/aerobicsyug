adb7c7e97926c25fab7928a8d0761769da899591
Lakshay Nagda <lakshya18n@gmail.com>
2026-03-22 15:55:15 +0530

    first commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -31,8 +31,8 @@ const Footer = () => {
                             <a href="https://instagram.com/aerobics_yug" target="_blank" rel="noopener noreferrer" className="social-icon" style={{ display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}>
                                 <img loading="lazy" src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg" alt="Instagram" style={{ width: "20px", height: "20px" }} />
                             </a>
-                            <a href="#" className="social-icon" style={{ color: "#FF0000", fontSize: '20px', display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}><FaYoutube /></a>
-                            <a href="#" className="social-icon" style={{ color: "#1877F2", fontSize: '20px', display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}><FaFacebook /></a>
+                            <a href="https://youtube.com/@aerobics5370?si=kezgKHdsR08OZVAQ" className="social-icon" style={{ color: "#FF0000", fontSize: '20px', display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}><FaYoutube /></a>
+                            <a href="https://www.facebook.com/share/1AkVt5Xjuj/?mibextid=wwXIfr" className="social-icon" style={{ color: "#1877F2", fontSize: '20px', display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}><FaFacebook /></a>
                         </div>
                     </div>
                 </div>
